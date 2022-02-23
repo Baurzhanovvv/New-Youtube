238c71c89a71c66e73177d958a4e941c30bbba46
Amir Baurzhanov <66216824+AmirBaurzhanov@users.noreply.github.com>
2022-02-23 20:14:22 +0600

    Update settings.py

--- new_youtube/settings.py
+++ new_youtube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-euaocdcsb$2r15^ecu34^+n60(vzt!qumc^%&t0g4b0o4sllvq'
+SECRET_KEY = '...secret_key...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
